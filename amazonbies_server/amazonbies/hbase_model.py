c7c50fc9bda9215c3c36b258b14e2237a5b40924
Little Leo <c2016.tw@gmail.com>
2014-10-20 22:51:57 -0400

    fix utf8 problem

--- amazonbies_server/amazonbies/hbase_model.py
+++ amazonbies_server/amazonbies/hbase_model.py
@@ -13,6 +13,14 @@ class HBase:
         table = self.conn.table(table)
         row_key = "%s_%s" %(userid, tweet_time_str)
         row = table.row(row_key)
+
+        if row.has_key('cfmain:tweetId'):
+            row['cfmain:tweetId'] = row['cfmain:tweetId'].decode("utf8","ignore")
+        if row.has_key('cfmain:sentimentScore'):
+            row['cfmain:sentimentScore'] = row['cfmain:sentimentScore'].decode("utf8","ignore")
+        if row.has_key("cfmain:censoredText"):
+            row["cfmain:censoredText"] = row["cfmain:censoredText"].decode("utf8", "ignore")
+
         return row
 
     def close(self):
