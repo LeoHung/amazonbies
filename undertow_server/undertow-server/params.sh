c2d6dbf087baa8be6287f02f249c38792a527b68
Ubuntu <ubuntu@ip-172-31-46-94.ec2.internal>
2014-11-03 04:23:23 +0000

    update params.hs

--- undertow_server/undertow-server/params.sh
+++ undertow_server/undertow-server/params.sh
@@ -2,4 +2,5 @@ export PORT=80
 export WARMUPQ1NUM=2000000
 export HBASEIP=172.31.42.123
 export WARMUPQ3FILE=data/q3.small.small
+export WARMUPQ4FILE=data/q4.small
 export MAVEN_OPTS="-Xms4g -Xmx6g"
