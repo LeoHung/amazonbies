8e36f774fb0837c653a84546173b6330cd5a680f
Little Leo <c2016.tw@gmail.com>
2014-11-14 15:21:51 -0500

    update params.sh

--- undertow_server/undertow-server/params.sh
+++ undertow_server/undertow-server/params.sh
@@ -1,6 +1,9 @@
-export PORT=80
+export PORT=8080
 export WARMUPQ1NUM=0
 export HBASEIP=null
+export MYSQLIP=54.86.62.157
 export WARMUPQ3FILE=data/q3.empty
-export WARMUPQ4FILE=data/q4.output
+export WARMUPQ4FILE=data/q4.empty
+export Q3SERVERIP=null
+export Q4SERVERIP=null
 export MAVEN_OPTS="-Xms4g -Xmx6g"
